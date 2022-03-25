4e2d8cddc140cbff811a1bb948442ddcbaeba8d2
Ashley Mikkola <ashleymikkola@Ashleys-MacBook-Pro.local>
2022-03-25 23:19:54 +0000

    adding the api and the tests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import styled, { ThemeProvider } from "styled-components";
+import React from 'react';
 import * as yup from "yup";
-import { Field, Form, Formik } from "formik";
-import { Button, TextField, crukTheme } from "@cruk/cruk-react-components";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import { Button, TextField, Select, crukTheme } from "@cruk/cruk-react-components";
+import axios from "axios";
 
 const SiteWrapper = styled.div`
   max-width: 1200px;
@@ -9,12 +11,50 @@ const SiteWrapper = styled.div`
   padding: 1rem;
 `;
 
-function App() {
-  const formSchema = yup.object().shape({
-    town: yup.string()
-      .required("Please enter keywords to search.")
-  });
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      n: 10,
+      disabled: false,
+      items: []
+  };
+  
+  this.getNasaData = this.getNasaData.bind(this);
+}
+
+getNasaData = (values) => {
+ this.setState({disabled: true, items: []})
+  axios.get(`https://images-api.nasa.gov/search?keywords=${values.keywords}&media_type=${values.mediaType}&year_start=${values.yearStart}`)
+    .then(data => {
+       let items = data.data.collection.items.slice(0, this.state.n);
+       for(var i=0; i < items.length; i++ ){
+         fetch(items[i].href)
+         .then(response => response.json())
+         .then( log => this.setState({items: [...this.state.items, log[0]], disabled: false}))
+       }
+       console.log(this.state.items.length)
+    });
+   
+ 
+  }
+
+ 
+
+render() {
+  const formSchema = yup.object().shape({
+    keywords: yup.string()
+      .min(2, 'Keywords must be between 2 and 50 characters.')
+      .max(50, 'Keywords must be between 2 and 50 characters.')
+      .required('Required'),
+    mediaType: yup.string()
+      .required('Please select a media type.'),
+    yearStart: yup.date().nullable()
+      .max(new Date().getFullYear(), "Year must not be in the future")
+      .typeError("Please enter a valid year.")
+  })
   return (
     <ThemeProvider theme={crukTheme}>
       <SiteWrapper>
@@ -26,12 +66,15 @@ function App() {
             validateOnChange
             initialValues={{
               keywords: "",
+              mediaType: "",
+              yearStart: "",
+
             }}
             validationSchema={formSchema}
             onSubmit={(values) => {
-              console.log(values);
+              this.getNasaData(values)
             }}>
-            {({ errors, touched }) => {
+            {() => {
               return (
                 <Form>
                   <Field name="keywords">
@@ -40,23 +83,49 @@ function App() {
                         <TextField
                           label="Keywords" 
                           type="text"
+                          isValid={true}
                           required
                           {...field}
                         />
-                        {errors.keywords && touched.keywords && <p>{errors.keywords}</p>}
+                        <ErrorMessage name="keywords"/>
                       </>
                     )}
                   </Field>
+                  <Field as={Select} label="Media Type" required name="mediaType">
+                      <option value="" selected disabled hidden></option>
+                      <option value="audio">Audio</option>
+                      <option value="video">Video</option>
+                      <option value="image">Image</option>
+                  </Field>
+                  <Field name="yearStart">
+                    {({ field }) => (
+                      <>
+                        <TextField
+                          label="Year start (optional)" 
+                          type="text"
+                          {...field}
+                        />
+                        <ErrorMessage name="yearStart"/>
+                      </>
+                    )}
+                  </Field>
+                  
 
-                  <Button type="submit">Submit</Button>
+                  <Button disabled={this.state.disabled} type="submit">{this.state.disabled ? "Submitting..." : "Submit"}</Button>
+                  
                 </Form>
+                
               )
             }}
           </Formik>
         </div>
+        <div>
+        <div>
+                {this.state.items.map((item, index) => { return <div><a href={item} target="_blank" rel="noreferrer">Result {index + 1} </a></div>})}
+                </div>
+        </div>
       </SiteWrapper>
     </ThemeProvider>
   );
-}
-
-export default App;
+ }
+} export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('example test', () => {
+test('the app renders and does not crash', () => {
   render(<App />);
-  const linkElement = screen.getByText(/some text here/i);
+  const linkElement = screen.getByText(/CRUK technical exercise - React/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+describe('NASA API', () => {
+  it('Gets details from the nasa api', async () => {
+    const answer = await fetch("https://images-api.nasa.gov/search?keywords=mars&media_type=image&year_start=2000")
+    // check status
+    expect(answer.status).toBe(200);
+  })
+  })
